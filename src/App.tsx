6659add4c1ebc5d0025d4e424a68674647a548ef
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-06-30 20:19:17 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='font-bold text-4xl text-center'>React Story Telling with TypeScript</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/redux/features/quizSlice.ts
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    
+}
+export const quizSlice = createSlice({
+    name : "quiz",
+    initialState,
+    reducers : {}
+})
+
+export default quizSlice.reducer;
