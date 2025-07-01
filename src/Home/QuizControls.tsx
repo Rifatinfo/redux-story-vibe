5c23b7aec3a415bed1d9c0306d59e82a17067197
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-07-01 21:00:28 +0600

    when user not click quiz section they not get to second step

--- src/Home/QuizControls.tsx
+++ src/Home/QuizControls.tsx
@@ -1,10 +1,23 @@
 import { Button } from "@/components/ui/button";
+import { nextQuestion, previousQuestion } from "@/redux/features/quizSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 
 export const QuizControls = () => {
+    const dispatch = useAppDispatch();
+    const { currentQuestionIndex , userAnswer} = useAppSelector((state) => state.quiz)
+    const isAnswerSelected = userAnswer[currentQuestionIndex] !== null;
+    const handleNext = () =>{
+          if(isAnswerSelected){
+              dispatch(nextQuestion());
+          }
+    }
+    const handlePrevious = () =>{
+          dispatch(previousQuestion());
+    }
     return (
         <div className="flex justify-between mt-3 px-6">
-            <Button>Previous</Button>
-            <Button>Next</Button>
+            <Button onClick={handlePrevious}>Previous</Button>
+            <Button onClick={handleNext}>Next</Button>
         </div>
     );
 };
