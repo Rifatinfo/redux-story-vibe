0c33047c3926a6696b0c05d3f50314d0707ad241
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-07-03 12:20:39 +0600

    add progress bar color

--- src/Home/QuizSummery.tsx
+++ src/Home/QuizSummery.tsx
@@ -5,13 +5,36 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 import { Progress } from "@/components/ui/progress";
-// import { useAppSelector } from "@/redux/hooks";
+import { useAppSelector } from "@/redux/hooks";
 
 const QuizSummery = () => {
-    // const {questions, userAnswer} = useAppSelector((state) => state.quiz);
-    // const correctionAnswerCount = questions.reduce((count, questions, idx) => {
-    //     return questions.correctAnswer === userAnswer[idx] ? count + 1 : count ;
-    // })
+    const { questions, userAnswer } = useAppSelector((state) => state.quiz);
+    const correctionAnswerCount = questions.reduce((count, questions, idx) => {
+        return questions.correctAnswer === userAnswer[idx] ? count + 1 : count;
+    }, 0);
+
+    const incorrectAnswerCount = questions.length - correctionAnswerCount;
+    const correctPercentage: number = parseFloat(
+        ((correctionAnswerCount / questions.length) * 100).toFixed(2)
+    );
+    console.log(incorrectAnswerCount, correctPercentage, correctionAnswerCount);
+
+    const getPerformance = (percentage : number) =>{
+        if(percentage >= 90){
+           return {
+            rating : "Excellent",
+            color : "bg-green-800"
+           }
+        } else{
+            return {
+            rating : "Try to better",
+            color : "bg-red-800"
+           }
+        }
+    }
+
+    const {rating, color} = getPerformance(correctPercentage)
+    
     return (
         <div>
             <Card>
@@ -19,15 +42,15 @@ const QuizSummery = () => {
                     <CardTitle>Quiz Summary</CardTitle>
                 </CardHeader>
                 <CardContent>
-                    <p>You Got 4 Out of 10</p>
+                    <p>You Got {correctionAnswerCount} Out of {questions.length}</p>
                     {/* progress bar */}
-                    <Progress value={33} />
+                    <Progress value={correctPercentage} className={`${color}`} />
                     <div>
-                        <span>40%</span>
-                        <span>Performance : Good</span>
+                        <span>{correctPercentage}%</span>
+                        <span>Performance : {rating}</span>
                     </div>
                     <div>
-                        <strong>Incorrect Answer </strong> <span>6</span>
+                        <strong>Incorrect Answer </strong> <span>{incorrectAnswerCount}</span>
                     </div>
                     <div>
                         <p>Great Job keep Practicing</p>
